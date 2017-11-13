fc6adfe932c4e11417f249084d6f6328982c95f4
Alfonso Wong <Awong@Alfonsos-MacBook-Pro.local>
2017-11-12 23:23:25 -0800

    adds initial project files

---
+++ server/app.js
@@ -0,0 +1,20 @@
+// import files and packages up here
+var express = require("express");
+var morgan = require("morgan");
+var topSpots = require("./data.json");
+
+// create your express server below
+var app = express();
+
+// add your routes and middleware below
+
+app.use(morgan("dev"));
+
+app.get('/', function(req, res){
+    res.send("");
+});
+app.get("/data", function(req,res){
+    res.send(topSpots);
+});
+// finally export the express application
+module.exports = app;
